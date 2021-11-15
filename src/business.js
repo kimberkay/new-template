764ed4fe3d69f8c232984fc880fc708d3f0a5665
Kim Brannian <brannian@gmail.com>
2021-11-15 09:56:48 -0800

    create template

--- src/business.js
+++ src/business.js
@@ -1,24 +1,3 @@
 
-export default function PizzaOrder(size, toppings) {
-    this.size = size;
-    this.toppings = toppings;
-  }
-
-  PizzaOrder.prototype.addToppings = function() {
-    let toppingsCost = this.toppings.length * 1.25;
-    return toppingsCost;
-  }
-
-  PizzaOrder.prototype.pizzaCost = function() {
-    let baseCost = 0;
-    if (this.size === "medium") {
-      baseCost = 24;
-    } else if (this.size === "large") {
-      baseCost = 29;
-    } else {
-    }
-    return baseCost + this.addToppings();
-  
-  };
 
 
